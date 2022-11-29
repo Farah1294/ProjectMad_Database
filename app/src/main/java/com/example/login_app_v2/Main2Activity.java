662939b1d750122cbee4a000f98e10a64bd4e103
Farah Wahida <farahwahida1294@gmail.com>
2022-11-30 06:01:22 +0800

    1

--- app/src/main/java/com/example/login_app_v2/Main2Activity.java
+++ app/src/main/java/com/example/login_app_v2/Main2Activity.java
@@ -3,12 +3,17 @@ package com.example.login_app_v2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        username = (TextView) findViewById(R.id.email_reset);
     }
 }
